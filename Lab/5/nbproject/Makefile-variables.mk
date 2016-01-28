6a5f6ac1399c407c2ad6374ea60aa01f51fdd24d
davmac909 <davmac909@gmail.com>
2016-01-28 11:39:55 -0800

    Signed-off-by: davmac909 <davmac909@gmail.com>

--- Lab/5/nbproject/Makefile-variables.mk
+++ Lab/5/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=5
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/5
+CND_ARTIFACT_NAME_Debug=6
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/6
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=5.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/5.tar
+CND_PACKAGE_NAME_Debug=6.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/6.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=5
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/5
+CND_ARTIFACT_NAME_Release=6
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/6
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=5.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/5.tar
+CND_PACKAGE_NAME_Release=6.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/6.tar
 #
 # include compiler specific variables
 #
